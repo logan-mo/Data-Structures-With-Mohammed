3792ebc1ac7910d078f7d46ad7cd3865028600ff
Muhamad Luqman <itsluqman55@gmail.com>
2023-06-16 18:07:09 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// C++ Supports Object Oriented PRogramming
+// IT's worse than C
+
+#include <iostream>
+using namespace std;
+
+int swap(int a, int b){
+    int c = a;
+    a = b;
+    b = c;
+    return ;
+}
+
+int main(){
+
+    int a = 10, b = 20;
+
+    // swap function 
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
